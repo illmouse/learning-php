6d3e5332df570989289397b54b4dbbc25bb3c531
Nikita Denisov <ndenisov@crocs.com>
2017-10-27 18:41:18 +0300

    Lesson 1

---
+++ lesson1/index.php
@@ -0,0 +1,71 @@
+<?php
+	$h1 = "<h1>minimalistica</h1>";
+	$title = "<title>minimalistica</title>";
+	$body = "<p>Nunc eget nunc libero. Nunc commodo euismod massa quis vestibulum. Proin mi nibh, dignissim a pellentesque at, ultricies sit amet sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel lorem eu libero laoreet facilisis. Aenean placerat, ligula quis placerat iaculis, mi magna luctus nibh, adipiscing pretium erat neque vitae augue. Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at ipsum.</p>";
+	$col1 = "<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>";
+	$col2 = "<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>";
+	$coll = "<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="author" content="Luka Cvrk (www.solucija.com)" />
+	<link rel="stylesheet" href="css/main.css" type="text/css" />
+	<?php
+		echo $title;
+	?>
+</head>
+<body>
+	<div id="content">
+		<?php
+			echo $h1;
+		?>
+		<ul id="menu">
+			<li><a href="#">home</a></li>
+			<li><a href="#">archive</a></li>
+			<li><a href="#">contact</a></li>
+		</ul>
+	
+		<div class="post">
+			<div class="details">
+				<h2><a href="#">Nunc commodo euismod massa quis vestibulum</a></h2>
+				<p class="info">posted 3 hours ago in <a href="#">general</a></p>
+			</div>
+			<div class="body">
+				<?php
+					echo $body;
+				?>
+			</div>
+			<div class="x"></div>
+		</div>
+		
+		<div class="col">
+			<h3><a href="#">Ut enim risus rhoncus</a></h3>
+				<?php
+					echo $col1;
+				?>
+			<p>&not; <a href="#">read more</a></p>
+		</div>
+		<div class="col">
+			<h3><a href="#">Maecenas iaculis leo</a></h3>
+				<?php
+					echo $col2;
+				?>
+			<p>&not; <a href="#">read more</a></p>
+		</div>
+		<div class="col last">
+			<h3><a href="#">Quisque consectetur odio</a></h3>
+				<?php
+					echo $coll;
+				?>
+			<p>&not; <a href="#">read more</a></p>
+		</div>
+		
+		<div id="footer">
+			<p>Copyright &copy; <em>minimalistica</em> &middot; Design: Luka Cvrk, <a href="http://www.solucija.com/" title="Free CSS Templates">Solucija</a></p>
+		</div>
+	</div>
+</body>
+</html>
